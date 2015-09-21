241636a61d003c3061dcea5925269b8d069afe02
DanielAviv <daniel_avivnotario@hotmail.com>
2015-09-20 22:06:54 -0400

    Terminado keyframe
    Se arreglaron ultimos arreglos a keyframe, ahora calcula un archivo de
    output y sólo guarda los frames si uno se lo pide

---
+++ Source/detection.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+This executable detects the faces of the characters in the frames determined by
+keyframe.py.
+"""
+
+#import sys
+#from os import listdir
+#from os.path import isfile, join, splitext
+#import argparse
+
+import cv2
+
+__author__ = "Daniel Aviv"
+__credits__ = "Juan Manuel Barrios"
+__email__ = "daniel_avivnotario@hotmail.com"
+__status__ = "Development"
+
+def main(argv=None):
+	return 0
+
+if __name__ == "__main__":
+	main()
