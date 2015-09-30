a1bfdc106d9c88d793592dca7781afead6d29630
DanielAviv <daniel_avivnotario@hotmail.com>
2015-09-30 19:52:49 -0400

    Rename de un file

---
+++ Source/feature_extraction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+import cv2
+
+__author__ = "Daniel Aviv"
+__credits__ = "Juan Manuel Barrios"
+__email__ = "daniel_avivnotario@hotmail.com"
+__status__ = "Development"
+
+def main(argv=None):
+	pass
+	
+if __name__ == "__main__":
+	main()
