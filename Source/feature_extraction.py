436570f19b4343222827a344010f0a1fd5d24826
DanielAviv <daniel_avivnotario@hotmail.com>
2015-10-13 20:47:02 -0300

    Terminado primer descriptor
    Se terminó el cálculo pero no el guardado del primer descriptor
    histogramas de tono

--- Source/feature_extraction.py
+++ Source/feature_extraction.py
@@ -7,6 +7,7 @@ import sys
 import argparse
 
 import cv2
+from numpy import concatenate
 
 __author__ = "Daniel Aviv"
 __credits__ = "Juan Manuel Barrios"
@@ -46,9 +47,10 @@ def calc_descriptor(data_dictionary, video_path):
 		video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frame_pos)
 		ret, frame = video.read()
 			
-		for face in faces[0:1]:
+		for face in faces[:1]:
 			x, y, w, h = face
-			histogram = hue_histogram(frame[y:y+h, x:x+w])
+			#histogram = hue_histogram(frame[y:y+h, x:x+w], 32)
+			#histogram = hue_histogram_zone(frame[y:y+h, x:x+w], 32)
 			
 		video.release()
 		break
@@ -57,9 +59,40 @@ def calc_descriptor(data_dictionary, video_path):
 
 """
 """
-def hue_histogram(image):
+def hue_histogram(image, bins):
 	cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	cv2.calcHist([image], [0], None, [256], [0,179])
+	hist = cv2.calcHist([image], [0], None, [bins], [0,179])
+	hist_normed = hist/sum(hist)
+	
+	return hist_normed
+	
+"""
+"""
+def hue_histogram_zone(image, bins):
+	cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	rows, cols, channels = image.shape
+	
+	a = image[:rows/2, :cols/2]
+	b = image[rows/2:, :cols/2]
+	c = image[:rows/2, cols/2:]
+	d = image[rows/2:, cols/2:]
+	
+	hist_a = cv2.calcHist([a], [0], None, [bins], [0,179])
+	hist_b = cv2.calcHist([b], [0], None, [bins], [0,179])
+	hist_c = cv2.calcHist([c], [0], None, [bins], [0,179])
+	hist_d = cv2.calcHist([d], [0], None, [bins], [0,179])
+	
+	hist_a_normed = hist_a/sum(hist_a)
+	hist_b_normed = hist_b/sum(hist_b)
+	hist_c_normed = hist_c/sum(hist_c)
+	hist_d_normed = hist_d/sum(hist_d)
+	
+	return concatenate((hist_a_normed, hist_b_normed, hist_c_normed, hist_d_normed), axis=0)
+
+"""
+"""
+def rand_patch(image, amount, size):
+	pass
 
 def main(argv=None):
 	input_path = INPUT_PATH
--- Source/test/feature_test.py
+++ Source/test/feature_test.py
@@ -4,19 +4,39 @@
 This test-case tests the module feature_extraction.py.
 """
 
-import unittest
-
 import feature_extraction
+
+import unittest
+from os.path import join
+
+import cv2
 
 __author__ = "Daniel Aviv"
 __credits__ = "Juan Manuel Barrios"
 __email__ = "daniel_avivnotario@hotmail.com"
 __status__ = "Development"
 
+#
+COLOR_SPACE_IMAGE_PATH = join("test", "test_image.png")
+
 class TestFeatures(unittest.TestCase):
-	def test_1(self):
-		#self.assertEqual('foo'.upper(), 'FOO')
-		pass
+	def setUp(self):
+		self.color_space_image = cv2.imread(COLOR_SPACE_IMAGE_PATH, 1)
+		
+	def tearDown(self):
+		self.color_space_image = None
+	
+	def test_hsv(self):
+		hsv_image = cv2.cvtColor(self.color_space_image, cv2.COLOR_BGR2HSV)
+		hue,sat,val = cv2.split(hsv_image)
+		
+		for row in hue:
+			for value in row:
+				self.assertTrue((value >= 0) and (value < 180))
+	
+	def test_hue_histogram(self):
+		histogram = feature_extraction.hue_histogram(self.color_space_image, 16)
+		self.assertTrue(len(histogram) == 16)
 
 if __name__ == "__main__":
 	unittest.main()
