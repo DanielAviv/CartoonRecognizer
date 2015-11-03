e0af244760089817944026c588f507b3e5fc4218
DanielAviv <daniel_avivnotario@hotmail.com>
2015-11-03 18:03:41 -0300

    Terminado desarrollo!
    Terminé el desarrollo de la función de distancia y la votación

--- Source/distance.py
+++ Source/distance.py
@@ -9,6 +9,7 @@ import file_management as fm
 from os import listdir
 from os.path import isfile, join, basename
 import argparse
+from operator import itemgetter
 
 import cv2
 import numpy as np
@@ -28,55 +29,60 @@ INPUT_DIRECTORY = "D:\\Mis Documentos\\MaterialU\\Memoria\\CartoonRecognizer\\Da
 #
 MATCHES = 5
 
+#
+FINALISTS = 5
+
 """
 """
 def kNN_FLANN(input_features, feature_files, desired_matches):
-	FLANN_INDEX_KDTREE = 0
-	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-	search_params = dict(checks = 100)
-	
-	input_as_array = np.asarray(input_features, np.float32)
-	flann = cv2.flann_Index(input_as_array, index_params)
-	
-	candidates = []
-	
-	for file_path in feature_files:
-		dataset_features_dict = fm.load_file(file_path)
-		dataset_features = dataset_features_dict.values()
-		
-		#input_as_array = np.asarray(input_features, np.float32)
-		data_as_array = np.asarray(dataset_features, np.float32)
-		print "k: " + str(desired_matches)
-		matches, dist = flann.knnSearch(data_as_array, 5, params=search_params)
-		print len(matches)
-		#print input_as_array
-		break
-	
-	return 0
+	return None
 	
 """
 """
 def kNN_scikit(input_features, feature_files, desired_matches):
-	pass
+	return None
 	
 """
 """
 def kNN_OCV(input_features, data_features, desired_matches):
-	result = []
+	data_features_value = [ data[1] for data in data_features ]
+	data_as_array = np.asarray(data_features_value, np.float32)
 	responses = np.asarray(range(0, len(data_features)), np.float32)
-
+	
 	knn = cv2.KNearest()
-	knn.train(data_features, responses)
+	knn.train(data_as_array, responses)
 	ret, results, neighbours, dist = knn.find_nearest(input_features, desired_matches)
 	
 	candidates_position_array = []
+	candidates_distance_array = []
 	
 	for neighbour_array in neighbours:
 		candidates_position_array.extend(neighbour_array)
 		
-	result = [ data_features[position] for position in candidates_position_array ]
+	for distance_array in dist:
+		candidates_distance_array.extend(distance_array)
 	
-	return result
+	result = [ data_features[int(position)] for position in candidates_position_array ]
+	
+	return result, candidates_distance_array
+	
+"""
+"""
+def vote(candidates, nb_of_finalists):
+	votes = {}
+	
+	for candidate in candidates:
+		face_key = candidate[0][0]
+		votes[face_key] = 0
+	
+	for candidate in candidates:
+		face_key = candidate[0][0]
+		votes[face_key] = votes[face_key] + (10/candidate[1])
+	
+	real_nb = min(nb_of_finalists, len(candidates))
+	best = dict(sorted(votes.iteritems(), key=itemgetter(1), reverse=True)[:nb_of_finalists])
+
+	return best
 	
 """
 """
@@ -111,24 +117,22 @@ def main(argv=None):
 		input_features = compute_input_features(input_images)
 		input_as_array = np.asarray(input_features, np.float32)
 		
-		candidates = {}
+		candidates = []
 		
 		for file_path in feature_files:
 			dataset_features_dict = fm.load_file(file_path)
-			dataset_features = dataset_features_dict.values()
-			data_as_array = np.asarray(dataset_features, np.float32)
+			dataset_features = dataset_features_dict.items()
 			
-			selected_features = kNN_OCV(input_as_array, data_as_array, MATCHES)
-			print len(selected_features)
-
-			for key, value in dataset_features_dict.items():
-				for feature in selected_features:
-					if np.array_equal(feature, value):
-						new_key = basename(file_path) + ":" + key
-						candidates[new_key] = value
-						break
-
-		print len(candidates.keys()) == len(set(candidates.keys()))
+			selected_features, distances = kNN_OCV(input_as_array, dataset_features, MATCHES)
+			
+			for feature in selected_features:
+				new_key = basename(file_path) + ":" + feature[0]
+				candidates.append([new_key, feature[1]])
+				
+		final_candidates, distance = kNN_OCV(input_as_array, candidates, MATCHES)
+		matches = zip(final_candidates, distance)
+		
+		final_results = vote(matches, FINALISTS)
 
 	except IOError:
 		print "ERROR: The path provided does not exist"
